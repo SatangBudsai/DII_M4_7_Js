f5745f1ac7899be187552fba7a601c7b34c8adb4
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-17 14:23:51 +0700

    fibonancci for and while

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ let n =10;
 before =0;
 after=1;
 if(n>0){
+    console.log("for fibonacci:");
     console.log(before);
     for(i=1;i<n;i++){
     console.log(after);
     cal = before + after;
     before = after;
     after = cal;
+    }
+    before =0;
+    after=1;
+    console.log("while fibonacci:");
+    console.log(before);
+    while(1<n){
+    console.log(after);
+    cal = before + after;
+    before = after;
+    after = cal;
+    n--;
     }
 }
