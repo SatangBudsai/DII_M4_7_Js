f65a128a657ed76d45a20250544ff6b23ea20cfd
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-17 15:00:18 +0700

    prime number

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-let n =10;
-before =0;
-after=1;
-if(n>0){
-    console.log("for fibonacci:");
-    console.log(before);
-    for(i=1;i<n;i++){
-    console.log(after);
-    cal = before + after;
-    before = after;
-    after = cal;
-    }
-    before =0;
-    after=1;
-    console.log("while fibonacci:");
-    console.log(before);
-    while(1<n){
-    console.log(after);
-    cal = before + after;
-    before = after;
-    after = cal;
-    n--;
+let n = 91;
+let check = true;
+for(i=2;i<n;i++){
+    if(n%i == 0){
+        check = false;
+        break;
     }
 }
+console.log(n+" Prime number ? : "+check);
+let count=2;
+while(count < n){
+    if(n%i == 0){
+        check = false;
+        break;
+    }
+    count++;
+}
+console.log(n+" Prime number ? : "+check);
