1bc0ecea94265c7d6d6f24f6e68fc38d8711a155
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-17 13:33:18 +0700

    if-else
    new

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ if (score >= 80){
     grade = 'A';
 }else if (score >= 75){
     grade ='B+';
-}else if (score >= 75){
+}else if (score >= 70){
     grade ='B';
-}else if (score >= 75){
+}else if (score >= 65){
     grade ='C+';
 }
 console.log(grade+' '+score)
