30d8d3b369e20024b1d5266f2570f24270946520
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-18 11:11:04 +0700

    collectioin mix array

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ let cenfest = {
 }
 console.log(cmu);
 console.log(cmu.name+' '+cenfest.name);
+console.log(cmu['name']===cmu.name);
+
+let place = [cmu, maya, cenfest]
+console.log(place)
+
+// collection mix array
+let place2 = [3];
+
+place2[0] = {
+    'name':'chiang main university',
+    'lat' : 18.8046735,
+    'long':98.9528446,
+}
+place2[1] = {
+    'name':'maya',
+    'lat' : 18.8046735,
+    'long':98.9528446,
+}
+place2[2] = {
+    'name':'central chiang mai',
+    'lat' : 18.8046735,
+    'long':98.9528446,
+}
+console.log(place2[0].name)
+console.log(place2[1].name)
+console.log(place2[2].name)
