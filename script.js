7332abf743009db16568c5375884c1eda135c083
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-05-18 10:59:04 +0700

    object oriented 01

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let n = 97;
-for(i=2;i<=n;i++){
-    check = true;
-    for(j=2;j<i;j++){
-        if(i%j == 0){
-            check=false;
-            break;
-        }
-    }
-    if(check == true){
-        console.log(i+" prime number");
-    }
+let cmu = {
+    'name':'chiang main university',
+    'lat' : 18.8046735,
+    'long':98.9528446,
 }
+let maya = {
+    'name':'maya',
+    'lat' : 18.8046735,
+    'long':98.9528446,
+}
+let cenfest = {
+    'name':'central chiang mai',
+    'lat' : 18.8046735,
+    'long':98.9528446,
+}
+console.log(cmu);
+console.log(cmu.name+' '+cenfest.name);
